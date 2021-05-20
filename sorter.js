1c1fcc3069f8335faffb2c236ee072cd2ca72d0a
pradeepkumarsm <pradeepkumar.sm999@gmail.com>
2021-05-20 19:25:41 +0530

    Changes

--- sorter.js
+++ sorter.js
@@ -1,27 +1,7 @@
-import {get} from 'lodash';
-
 const sortData = {
 	getSortedData : function(data, sortType, sortOrder, key){
         var actualKey = key;
-        if(Array.isArray(key)){
-            var cloneKey = key.slice();
-            actualKey = cloneKey.pop();
-        }
-
-		data.sort((param1, param2) => {
-            if(Array.isArray(key)){
-               param1 = _.get(param1, cloneKey);
-               param2 = _.get(param2, cloneKey);
-            }
-            switch(sortType){
-                case "Number":
-                    return ((param1 && param1[actualKey])? param1[actualKey] : 0) - ((param2 && param2[actualKey])? param2[actualKey] : 0);
-                    break;
-                default:
-                    return ((param1 && param1[actualKey])? param1[actualKey] : "") .localeCompare((param2 && param2[actualKey])? param2[actualKey] : "");
-                break;
-            }
-        });
+        console.log('Inside data sorter');
 
         if(sortOrder === "DSC") {
             data.reverse();
@@ -29,6 +9,5 @@ const sortData = {
         return data;
 	}
 };
-
 
 export default sortData;
