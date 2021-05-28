825f17263615be3a1d5952936a19f4b62b96b34d
pradeepkumarsm <pradeepkumar.sm999@gmail.com>
2021-05-28 17:23:24 +0530

    Removing console log

--- sorter.js
+++ sorter.js
@@ -3,7 +3,6 @@ const piWorker = new Worker('./worker.js', { type: 'module' });
 const sortData = {
 	getSortedData : function(data, sortType, sortOrder, key){
         var actualKey = key;
-        console.log('Inside data sorter');
 
         piWorker.postMessage(42);
 
