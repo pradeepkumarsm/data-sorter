03d77be26310d95a2ea07a25074bcd8c277699a9
pradeepkumarsm <pradeepkumar.sm999@gmail.com>
2026-06-20 16:15:31 +0530

    From local

--- sorter.js
+++ sorter.js
@@ -3,6 +3,7 @@ const piWorker = new Worker('./worker.js', { type: 'module' });
 const sortData = {
 	getSortedData : function(data, sortType, sortOrder, key){
         var actualKey = key;
+        
         piWorker.postMessage(42);
 		
         if(sortOrder === "DSC") {
